d31e2b2811849b5fcd8fe3bfa06669fb9c832b77
Mwesi <mwesi@yahoo.com>
2022-03-24 07:01:18 -0400

    Update planner.h

--- src/planner.h
+++ src/planner.h
@@ -4,52 +4,48 @@
 #include "planner.h"
 
 
-class Planner {
-	
-	
+class Planner 
+{
 	// Flag, if filter is initialized
 	bool is_initialized;
 	
+	
+	
 public:
 	
-	// Boolean to Check Leanes
-  bool RightLaneClearCheck;
-  bool LeftLaneClearCheck;
-  bool ThisLaneClearCheck
-	// Constructor
-	// @param M Number of particles
-	Planner() : is_initialized(false) {}
+  
+  	// Boolean to Check Leanes
+  	bool RightLaneClearCheck;
+  	bool LeftLaneClearCheck;
+  	bool ThisLaneClearCheck
+  
+  	// Constructor
+  	Planner() : is_initialized(false) {}
 
-	// Destructor
-	~Planner() {}
+  	// Destructor
+  	~Planner() {}
 
-	/**
-	 */
-	/**
-	 * initialized Returns whether particle filter is initialized yet or not.
-	 */
- void init(bool RightLaneClearCheck, bool LeftLaneClearCheck, bool ThisLaneClearCheck, double dist_inc, vector<double> next_x_vals, vector<double> next_y_vals);
+ 	void init(bool RightLaneClearCheck, bool LeftLaneClearCheck, bool ThisLaneClearCheck, double dist_inc, vector<double> next_x_vals, vector<double> next_y_vals);
  
- void predict(double s, int prev_size, vector<vector<double>> sensor_fusion);
+ 	void predict(double s, int prev_size, vector<vector<double>> sensor_fusion);
   
- void predict(double ego_s, int prev_size, vector<double> sensor_fusion); 
+ 	void predict(double ego_s, int prev_size, vector<double> sensor_fusion); 
   
- void straight(double dist_inc, vector<double> next_x_vals, vector<double> next_y_vals); 
+ 	void straight(double dist_inc, vector<double> next_x_vals, vector<double> next_y_vals); 
   
- void distance(double x1, double y1, double x2, double y2);
+ 	void distance(double x1, double y1, double x2, double y2);
   
- void getFrenet(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y);
+ 	void getFrenet(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y);
   
- void NextWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y);
+ 	void NextWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y);
   
- void predict(double s, int prev_size, vector<vector<double>> sensor_fusion);
+ 	void predict(double s, int prev_size, vector<vector<double>> sensor_fusion);
  
 	const bool initialized() const 
- {
-  return is_initialized;
+ 	{
+  
+		return is_initialized;
 	}
 };
-
-
 
 #endif /* PLANNER_H_ */
